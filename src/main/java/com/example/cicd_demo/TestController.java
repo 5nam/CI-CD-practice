ecb7faf3c1cc997e297395abe3d5d649d0e1c067
5nam <ohnam90@gmail.com>
2024-08-22 11:41:09 +0900

    response 메시지 변경

--- src/main/java/com/example/cicd_demo/TestController.java
+++ src/main/java/com/example/cicd_demo/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
 
     @GetMapping("/")
     public ResponseEntity<String> getCicd() {
-        return ResponseEntity.ok("TEAM PROJECT CI/CD PRACTICE");
+        return ResponseEntity.ok("TEAM PROJECT CI/CD");
     }
 
 }
